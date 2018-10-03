0dde4615d2025e3447cfc0b957a561f5ec2dbdef
Ragab <hamadar1996@gmail.com>
2018-10-03 20:28:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req , res , next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+
+    fs.appendFileSync('server log' , log + '\n') ;
+    next();
+});
+
+app.use((req , res , next) => {
+    res.render('mentainance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getcurrentyear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamit', (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req ,res) => {
+    //res.send ('<h1>Hello Express!</h1>');
+    res.render('home.hbs',{
+        welcome : 'Hello To My Page!',
+        pagetitle: 'Home Page',
+    });
+});
+
+app.get('/about', (req , res) => {
+    res.render('about.hbs',{
+        pagetitle: 'About Page',
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is working!')
+});
