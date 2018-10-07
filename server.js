5656c3baea0fd95a18a9417b3154aecf846b0d8f
Ragab <hamadar1996@gmail.com>
2018-10-07 21:07:33 +0200

    adding projects view page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about', (req , res) => {
     });
 });
 
+app.get('/projects', (req , res) => {
+    res.render('projects.hbs',{
+        pagetitle: 'Portfolio Page',
+    });
+});
+
 app.listen(port, () => {
     console.log(`server is working on port ${port}!`);
 });
