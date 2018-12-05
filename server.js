14c86656e8db3e513c498bae77202b46d5b97292
ahmedragab96 <hamadar1996@gmail.com>
2018-12-05 17:20:17 +0200

    adding bootstrap styling

--- server.js
+++ server.js
@@ -6,22 +6,36 @@ const port = process.env.PORT || 3000; //port configuration for heroku
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+
+// setting up the view engine Handlebarsjs
 app.set('view engine', 'hbs');
 
-
+//registering a middleware to save all of the log requests to server
 app.use((req , res , next) => {
     var now = new Date().toString();
     var log = `${now} : ${req.method} ${req.url}`;
 
-    fs.appendFileSync('server log' , log + '\n') ;
+    fs.appendFileSync('server log' , log + '\n' , (error) => {
+        if (error){
+            console.log("Unable to append to server.log")
+        }
+    }) ;
     next();
 });
 
-/*app.use((req , res , next) => {
-    res.render('mentainance.hbs');
-});*/
+// middleware to be executed when website 
+// is in mentainance mode
+
+// app.use((req , res , next) => {
+//     res.render('mentainance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
+
+
+//Redirect Bootstrap JS and CSS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
 
 hbs.registerHelper('getcurrentyear', () => {
     return new Date().getFullYear();
@@ -33,8 +47,8 @@ hbs.registerHelper('screamit', (text) => {
 
 
 app.get('/', (req ,res) => {
-    //res.send ('<h1>Hello Express!</h1>');
-    res.render('home.hbs',{
+    // res.send ('<h1>Hello Express!</h1>');
+        res.render('home.hbs',{
         welcome : 'Hello To My Page!',
         pagetitle: 'Home Page',
     });
